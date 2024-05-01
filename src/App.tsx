0e9301ec90fd7468572e1ca7188ea5e40dc8afd1
King Chan <king.mementomori@gmail.com>
2024-05-01 16:10:06 +0800

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+function App() {
+  return <div className="App">Hello World</div>;
+}
+
+export default App;
